8d589afedc5a29f4361ae09bbb0ad7c030d82eb2
Atanas Vasilev <atanas.vasilev@gmail.com>
2017-06-08 01:29:06 +0300

    Complete assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  odds = [];
+  evens = [];
+
+  onNumberGenerated(n: number) {
+    if (n % 2 === 0) {
+      this.evens.push(n);
+    } else {
+      this.odds.push(n);
+    }
+  }
 }
--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import Timer = NodeJS.Timer;
 
 @Component({
   selector: 'app-game-control',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  intervalRef: any;
+  @Output() numberGenerated = new EventEmitter<number>();
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onGameStart() {
+    let initialNumber = 1;
+    this.intervalRef = setInterval(() => {
+      this.numberGenerated.emit(initialNumber++);
+    }, 1000);
+  }
+
+  onGameEnd() {
+    clearInterval(this.intervalRef);
+  }
 }
